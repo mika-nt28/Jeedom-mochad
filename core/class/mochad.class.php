a15cbdd1dec858a12981050e05c6f9410aec609a
Halbout Michael <michaelhalbout@gmail.com>
2016-10-04 13:38:01 +0200

    Update mochad.class.php

--- core/class/mochad.class.php
+++ core/class/mochad.class.php
@@ -1,30 +1,7 @@
 <?php
-
-/* This file is part of Jeedom.
- *
- * Jeedom is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * Jeedom is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with Jeedom. If not, see <http://www.gnu.org/licenses/>.
- */
-
-/* * ***************************Includes********************************* */
 require_once dirname(__FILE__) . '/../../../../core/php/core.inc.php';
 
 class mochad extends eqLogic {
-    /*     * *************************Attributs****************************** */
-
-
-
-    /*     * ***********************Methode static*************************** */
 	public static function BusMonitor() {
 		$address=config::byKey('mochadHost', 'mochad');
 		$port=config::byKey('mochadPort', 'mochad');
@@ -32,36 +9,30 @@ class mochad extends eqLogic {
 		if (!$socket) 
 			throw new Exception(__("$errstr ($errno)", __FILE__));
 		log::add('mochad', 'debug', 'Démarrage du démon');
-		while (!feof($socket)) 
-			{ 
+		while (!feof($socket)) { 
 			$Ligne=stream_get_line($socket, 1000000,"\n");
 			log::add('mochad', 'debug', $Ligne);
 			$monitor['TxRx']=substr($Ligne, 15,2);
-			if (strrpos($Ligne,"RFSEC")>0)
-				{
+			if (strrpos($Ligne,"RFSEC")>0){
 				log::add('mochad', 'debug', 'Analyse de l\'équipement de sécurité');
 				$monitor['RfPl']=substr($Ligne, 18,5);
 				$monitor['Message']=substr($Ligne, 24);
 				self::X10SecurityAnalyse($monitor['Message']);
-				}
-			elseif (strrpos($Ligne,"House: ")>0 )
-				{
+			}elseif (strrpos($Ligne,"House: ")>0 )	{
 				log::add('mochad', 'debug', 'Analyse de l\'équipement');
 				$monitor['RfPl']=substr($Ligne, 18,2);
 				$monitor['Message']=substr($Ligne, 21);
 				$Lignedouble.='&'.$monitor['Message'];
 				self::X10Analyse($Lignedouble);
-				}
-			elseif (strrpos($Ligne,"HouseUnit: ")>0)
-				{
+			}elseif (strrpos($Ligne,"HouseUnit: ")>0){
 				$monitor['RfPl']=substr($Ligne, 18,2);
 				$monitor['Message']=substr($Ligne, 21);
 				$Lignedouble=$monitor['Message'];
-				}
-			self::addCacheMonitor($monitor);
 			}
-		fclose($socket); 
+			self::addCacheMonitor($monitor);
 		}
+		fclose($socket); 
+	}
 	public static function X10Analyse($Message) {
 		$Ligne=split('&',$Message);
 		$Etat=substr($Ligne[1],strrpos($Ligne[1],"Func: ")+6);	
@@ -74,17 +45,15 @@ class mochad extends eqLogic {
 		$Code=substr(substr($Ligne[0],strrpos($Ligne[0],"HouseUnit: ")+11),1,1);
 		log::add('mochad', 'debug', 'Recherche du module '.$Unite.$Code);
 		$Equipement = mochad::byLogicalId($Unite.'-'.$Code, 'mochad');
-		if (is_object($Equipement)) 
-			{
+		if (is_object($Equipement)) {
 			$State=$Equipement->getCmd('info','Etat_'.$Equipement->getId());
-			if (is_object($State)) 
-				{
+			if (is_object($State)) {
 				$State->setCollectDate('');
 				$State->event($EtatModule);
 				$State->save();
-				}
 			}
 		}
+	}
 	public static function X10SecurityAnalyse($Message) {
 		$Adresse=substr($Message,strrpos($Message,"Addr: ")+6,8);
 		$Fonction=split('_',substr($Message,strrpos($Message,"Func: ")+6));
@@ -95,8 +64,7 @@ class mochad extends eqLogic {
 		log::add('mochad', 'debug', 'Nom: '.$Fonction[count($Fonction)-1]);
 		log::add('mochad', 'debug', 'Adresse: '.$Adresse);
 		$Equipement = self::byLogicalId($Adresse, 'mochad');
-		if (!is_object($Equipement) && config::byKey('autoAddDevice', 'mochad')==1) 
-			{
+		if (!is_object($Equipement) && config::byKey('autoAddDevice', 'mochad')==1) {
 			log::add('mochad', 'debug', 'Création de l\'équipement de sécurité');
 			$Equipement = new mochad();
 			$Equipement->setName($Fonction[count($Fonction)-1]);
@@ -106,22 +74,20 @@ class mochad extends eqLogic {
 			$Equipement->setIsEnable(1);
 			$Equipement->setIsVisible(1);
 			$Equipement->save();
-			}	
-		if (is_object($Equipement)) 
-			{
+		}	
+		if (is_object($Equipement)) {
 			log::add('mochad', 'debug', 'L\'équipement de sécurité à été trouvé');
 			$Security=$Equipement->getCmd('info','Etat_'.$Adresse);
 			if (!is_object($Security) && config::byKey('autoAddDevice', 'mochad')==1) 
 				$Security=self::CreateMochadModuleSecurity($Equipement, $Fonction[count($Fonction)-1], $Adresse);
-			if (is_object($Security)) 
-				{
+			if (is_object($Security)) {
 				log::add('mochad', 'debug', 'Mise a jours de la valeur de la comande: '.$Valeur);
 				$Security->setCollectDate('');
 				$Security->event($Valeur);
 				$Security->save();
-				}
 			}
 		}
+	}
 	public static function addCacheMonitor($_monitor) {
 		$cache = cache::byKey('mochad::Monitor');
 		$value = json_decode($cache->getValue('[]'), true);
@@ -172,16 +138,14 @@ class mochad extends eqLogic {
 	public static function deamonRunning() {
 		$result = exec("ps aux | grep mochad | grep -v grep | awk '{print $2}'");
 		$cron = cron::byClassAndFunction('mochad', 'BusMonitor');
-     	if(is_object($cron)){
+		if(is_object($cron)){
 			if($result != ""&& $cron->getState()=="run")
 				return $result;
 		}
-        return false;
-    }
-    public static function cron() {
-		self::UpdateInfoModule();
-      }
-    public static function UpdateInfoModule() {
+		return false;
+	}
+
+	public static function UpdateInfoModule() {
 		foreach(eqLogic::byType('mochad') as $Equipement){
 			foreach($Equipement->getCmd('info') as $Commande){
 				$Commande->event($Commande->execute());
@@ -193,6 +157,7 @@ class mochad extends eqLogic {
 		$return = array();
 		$return['log'] = 'mochad_update';
 		$return['state'] = 'nok';
+		$return['progress_file'] = '/tmp/compilation_Mochad_in_progress';
 		if(file_exists('/etc/mochad/mochad_VERSION')){
 			if(exec("cat /etc/mochad/mochad_VERSION")=="v0.1.16")
 				$return['state'] = 'ok';
@@ -200,11 +165,11 @@ class mochad extends eqLogic {
 		return $return;
 	}
 	public static function dependancy_install() {
+		if (file_exists('/tmp/compilation_Mochad_in_progress')) {
+			return;
+		}
 		log::remove('mochad_update');
 		$cmd = 'sudo /bin/bash ' . dirname(__FILE__) . '/../../ressources/install-mochad.sh';
-		$cmd .= ' no_compil';
-		$cmd .= ' ' . network::getNetworkAccess('internal', 'proto:127.0.0.1:port:comp');
-		$cmd .= ' ' . config::byKey('api');
 		$cmd .= ' >> ' . log::getPathToLog('mochad_update') . ' 2>&1 &';
 		exec($cmd);
 	}
@@ -223,9 +188,8 @@ class mochad extends eqLogic {
 		log::remove('mochad');
 		exec('sudo mochad');
 		$deamon_info = self::deamon_info();
-		if ($deamon_info['launchable'] != 'ok') {
+		if ($deamon_info['launchable'] != 'ok') 
 			throw new Exception(__('Veuillez vérifier la configuration', __FILE__));
-		}
 		$cron = cron::byClassAndFunction('mochad', 'BusMonitor');
 		if (!is_object($cron)) {
 			$cron = new cron();
@@ -251,34 +215,27 @@ class mochad extends eqLogic {
 }
 
 class mochadCmd extends cmd {
-    public function preSave() {
+	public function preSave() {
 		if ($this->getConfiguration('MochadCommandeType') == '')
-            throw new Exception(__('Le type de commande ne peut etre vide', __FILE__));
-		if ($this->getConfiguration('MochadCommandeType') != 'rfsec'  && $this->getType() == 'info')
-			{
+			throw new Exception(__('Le type de commande ne peut etre vide', __FILE__));
+		if ($this->getConfiguration('MochadCommandeType') != 'rfsec'  && $this->getType() == 'info'){
 			$Equipement = mochad::byId($this->getEqLogic_id());
 			if (is_object($Equipement)) 
-				{
 				$this->setLogicalId('Etat_'.$Equipement->getId());
-				}
-			}
-    }
+		}
+	}
 	public function postSave() {
-		if ($this->getConfiguration('MochadCommandeType') != 'rfsec'  && config::byKey('autoAddDevice', 'mochad')==1)
-			{
+		if ($this->getConfiguration('MochadCommandeType') != 'rfsec'  && config::byKey('autoAddDevice', 'mochad')==1){
 			$Equipement = mochad::byId($this->getEqLogic_id());
-			if (is_object($Equipement)) 
-				{
+			if (is_object($Equipement)) {
 				$State=$Equipement->getCmd('info','Etat_'.$Equipement->getId());
 				if (!is_object($State)) 
-					{
 					mochad::CreateMochadModuleStat($Equipement);
-					}
-				}
 			}
+		}
 		if ($this->getConfiguration('MochadCommandeType') == '')
-            throw new Exception(__('Le type de commande ne peut etre vide', __FILE__));
-    } 
+			throw new Exception(__('Le type de commande ne peut etre vide', __FILE__));
+	} 
 	public function execute($_options = null){     
 		$address=config::byKey('mochadHost', 'mochad');
 		$port=config::byKey('mochadPort', 'mochad');
@@ -288,104 +245,93 @@ class mochadCmd extends cmd {
 		$codeEquipement=split('-',$Equipement->getLogicalId())[1];
 		//exemple de message : pl a all_lights_off
 		switch ($this->getType()) {
-            case 'action' :
-                switch ($this->getSubType()) {
-                    case 'slider':    
+			case 'action' :
+				switch ($this->getSubType()) {
+					case 'slider':    
 						$ActionValue = $_options['slider'];
-                        break;
-                    case 'color':
+					break;
+					case 'color':
 						$ActionValue = $_options['color'];
-                        break;
-                    case 'message':
+					break;
+					case 'message':
 						$ActionValue = $_options['message'];
-                        break;
-                    case 'other':		
-                        break;
-					}
-				switch ($this->getConfiguration('MochadCommande'))
-					{
+					break;
+				}
+				switch ($this->getConfiguration('MochadCommande')){
 					case 'on-off':	
 						$ActionValue=mochadCmd::ReadStatus($address,$port,$codeMaison,$codeEquipement);
-						if ($ActionValue==1)
-							{
+						if ($ActionValue==1){
 							$ActionValue=0;
 							$message = $this->getConfiguration('MochadCommandeType').' '.$codeMaison.$codeEquipement.' off';	
-							}
-						else
-							{
+						}else{
 							$ActionValue=1;
 							$message = $this->getConfiguration('MochadCommandeType').' '.$codeMaison.$codeEquipement.' on';	
-							}
-						if (is_object($State))
-							{
+						}
+						if (is_object($State)){
 							$State->setCollectDate('');
 							$State->event($ActionValue);
 							$State->save();
-							}
-						break;
+						}
+					break;
 					case 'on':	
 						$ActionValue=1;
 						$message = $this->getConfiguration('MochadCommandeType').' '.$codeMaison.$codeEquipement.' on';
-						if (is_object($State))
-							{
+						if (is_object($State)){
 							$State->setCollectDate('');
 							$State->event($ActionValue);
 							$State->save();
-							}
-						break;
+						}
+					break;
 					case 'off':	
 						$ActionValue=0;
 						$message = $this->getConfiguration('MochadCommandeType').' '.$codeMaison.$codeEquipement.' off';	
-						if (is_object($State))
-							{
+						if (is_object($State)){
 							$State->setCollectDate('');
 							$State->event($ActionValue);
 							$State->save();
-							}
-						break;
+						}
+					break;
 					case 'dim'://0-31
 						$message = $this->getConfiguration('MochadCommandeType').' '.$codeMaison.$codeEquipement.' '.$this->getConfiguration('MochadCommande').' '.$ActionValue;	
-						break;
+					break;
 					case 'bright'://0-31
 						$message = $this->getConfiguration('MochadCommandeType').' '.$codeMaison.$codeEquipement.' '.$this->getConfiguration('MochadCommande').' '.$ActionValue;
-						break;
+					break;
 					case 'xdim'://0-255	
 						$message = $this->getConfiguration('MochadCommandeType').' '.$codeMaison.$codeEquipement.' '.$this->getConfiguration('MochadCommande').' '.round($ActionValue*255/100);
-						break;
-					case 'all_lights_on':	
-						$message = $this->getConfiguration('MochadCommandeType').' '.$codeMaison.' '.$this->getConfiguration('MochadCommande');	
-						break;
+					break;
+						case 'all_lights_on':	
+					$message = $this->getConfiguration('MochadCommandeType').' '.$codeMaison.' '.$this->getConfiguration('MochadCommande');	
+					break;
 					case 'all_lights_off':
 						$message = $this->getConfiguration('MochadCommandeType').' '.$codeMaison.' '.$this->getConfiguration('MochadCommande');		
-						break;
+					break;
 					case 'all_units_off':
 						$message = $this->getConfiguration('MochadCommandeType').' '.$codeMaison.' '.$this->getConfiguration('MochadCommande');		
-						break;
-					}
-					# On ouvre le FLUX sur le CM15
-					$socket = stream_socket_client("tcp://$address:$port", $errno, $errstr, 100);
-					
-					if (!$socket) {
-							throw new Exception(__("$errstr ($errno)", __FILE__));
+					break;
+				}
+				# On ouvre le FLUX sur le CM15
+				$socket = stream_socket_client("tcp://$address:$port", $errno, $errstr, 100);
 
-					} else {
-						# ACTION X10 à réaliser
-						log::add('mochad', 'debug', 'Envoie : '.$message);
-						fwrite($socket, $message."\n");
+				if (!$socket) {
+					throw new Exception(__("$errstr ($errno)", __FILE__));
+				} else {
+					# ACTION X10 à réaliser
+					log::add('mochad', 'debug', 'Envoie : '.$message);
+					fwrite($socket, $message."\n");
 					$reponse='';
-					}
-
-					fclose($socket);
-                break;
+				}
+				fclose($socket);
+			break;
 			case 'info':
 				if ($this->getConfiguration('MochadCommandeType') == 'rfsec')
 					$reponse=$this->execCmd();
 				else
 					$reponse=mochadCmd::ReadStatus($address,$port,$codeMaison,$codeEquipement);
 				break;
-			}
-		return $reponse;
 		}
+		return $reponse;
+	}
 	public function ReadStatus($address,$port,$codeMaison,$codeEquipement)	{    
 		$socket = stream_socket_client("tcp://$address:$port", $errno, $errstr, 100);
 		fwrite($socket, "ST\n");
@@ -395,30 +341,23 @@ class mochadCmd extends cmd {
 		log::add('mochad', 'debug', 'Recus : '.$reponse);
 		$valeur=0;
 		$reponse=split("\n",$reponse);
-		foreach ($reponse as $Line)
-			{
-			if (strrpos($Line,"House ".$codeMaison)>0)
-				{
+		foreach ($reponse as $Line){
+			if (strrpos($Line,"House ".$codeMaison)>0){
 				log::add('mochad', 'debug', 'Table de valeur pour House '.$codeMaison.': '.$Line);
 				$Values=substr($Line,strrpos($Line,"House ".$codeMaison)+9);
 				log::add('mochad', 'debug', 'Table de valeur pour House '.$codeMaison.': '.$Values);
 				$Valuetable=split(',',$Values);
-				foreach ($Valuetable as $ValueDevice)
-					{
+				foreach ($Valuetable as $ValueDevice){
 					$ValueDevice=split('=',$ValueDevice);
-					if ($ValueDevice[0]==$codeEquipement)
-						{
+					if ($ValueDevice[0]==$codeEquipement){
 						$valeur=$ValueDevice[1];
 						log::add('mochad', 'debug', 'Valeur de l\'équipement '.$codeMaison.$codeEquipement.': '.$valeur);
-						}
 					}
 				}
 			}
-		return $valeur;
 		}
-   
-
-    /*     * **********************Getteur Setteur*************************** */
+		return $valeur;
+	}
 }
 
 ?>
